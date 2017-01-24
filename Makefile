27b6515ccdf274125a301041d9d2f457bcfdc7fb
ben-cooper <ben0cooper@gmail.com>
2017-01-23 21:54:32 -0500

    Completed FNV-1a hash for files.

---
+++ Makefile
@@ -0,0 +1,2 @@
+default: fnv.c
+	gcc -Wall -Wextra -Werror -Wpedantic -m64 fnv.c -o fnv
---
+++ fnv.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdint.h>
+#define FNVOFFSET 14695981039346656037U
+#define FNVPRIME 1099511628211
+
+uint64_t fnv(FILE *fp) {
+	int8_t in;
+	uint8_t chr;
+	uint64_t hash = FNVOFFSET;
+	while ((in = fgetc(fp)) != EOF) {
+		chr = (uint8_t) in;
+		hash ^= chr;
+		hash *= FNVPRIME;
+	}
+	return hash;
+}
+
+int main(int argc, char **argv) {
+	FILE *fp;
+
+	if (argc != 2) {
+		fprintf(stderr, "usage: %s input_file\n", argv[0]);
+		return 1;
+	}
+	
+	if (!(fp = fopen(argv[1], "r"))) {
+		perror("fopen");
+		return 1;
+	}
+
+	printf("%lu\n", fnv(fp));
+	return 0;
+}
