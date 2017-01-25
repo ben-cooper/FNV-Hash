78be6f06bf02e94421d9cf26eec4aaa538ba1494
ben-cooper <ben0cooper@gmail.com>
2017-01-24 22:42:16 -0500

    Added ability to specify the read buffer size
    as an argument

--- fnv.c
+++ fnv.c
@@ -3,7 +3,6 @@
 #include <stdlib.h>
 #define FNVOFFSET 14695981039346656037U
 #define FNVPRIME 1099511628211
-#define BUFLEN 131072
 
 void *emalloc(size_t size) {
 	void *result;
@@ -17,14 +16,14 @@ void *emalloc(size_t size) {
 }
 
 
-uint64_t fnv(FILE *fp) {
+uint64_t fnv(FILE *fp, size_t bufsize) {
 	size_t read = 0;
 	size_t i;
 
-	uint8_t *buffer = (uint8_t *) emalloc(BUFLEN);
+	uint8_t *buffer = (uint8_t *) emalloc(bufsize);
 	uint64_t hash = FNVOFFSET;
 
-	while ((read = fread(buffer, 1, BUFLEN, fp)) > 0) {
+	while ((read = fread(buffer, 1, bufsize, fp)) > 0) {
 		for (i=0; i < read; i++) {
 			hash ^= buffer[i];
 			hash *= FNVPRIME;
@@ -37,9 +36,11 @@ uint64_t fnv(FILE *fp) {
 
 int main(int argc, char **argv) {
 	FILE *fp;
+	char *str;
+	unsigned long size = 131072;
 
-	if (argc != 2) {
-		fprintf(stderr, "usage: %s input_file\n", argv[0]);
+	if ((argc != 2) && (argc != 3)) {
+		fprintf(stderr, "usage: %s input_file [buffer_size_in_bytes]\n", argv[0]);
 		return 1;
 	}
 	
@@ -47,8 +48,11 @@ int main(int argc, char **argv) {
 		perror("fopen");
 		return 1;
 	}
+	
+	if (argc == 3)
+		size = strtoul(argv[2], &str, 10);
 
-	printf("%lx\n", fnv(fp));
+	printf("%lx\n", fnv(fp, size));
 	fclose(fp);
 	return 0;
 }
